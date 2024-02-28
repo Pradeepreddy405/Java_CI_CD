9bfaa27b37b5f4cd0b5896b66bfe81651807448e
Pradeepreddy405 <62452776+Pradeepreddy405@users.noreply.github.com>
2024-02-28 17:00:16 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class helloworld {
     public static void main(String[] args) {
         // Write your code here
         int n = 5;
-        for (i=0; i<=n; i++)
+        for (i=0;i<=n;++)
             {    
                  System.out.println("Hello, World!");
             }
